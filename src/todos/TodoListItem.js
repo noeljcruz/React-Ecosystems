09ecb9e7dd69c01f72a1860a56c6ac30098bb295
Noel Cruz <noel.j.cruz@outlook.com>
2020-07-30 10:50:33 -0700

    Added Redux flow to mark todo list items as completed.

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './TodoListItem.css';
 
-const TodoListItem = ({ todo, onRemovePressed }) => (
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => (
     <div className="todo-item-container">
         <h3>{todo.text}</h3>
         <div className="buttons-container">
             {todo.isCompleted
                 ? null
-                : <button className="completed-button">Mark As Completed</button>}
+                : <button 
+                onClick={() => onCompletedPressed(todo.text)}
+                className="completed-button">Mark As Completed</button>}
             <button
                 onClick={() => onRemovePressed(todo.text)}
                 className="remove-button">Remove</button>
